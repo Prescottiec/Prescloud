eaec379dfc26a4677cdde6e057f7e1f1ad377397
Prescott <carlsonprescott@gmail.com>
2020-01-07 17:38:06 -0800

    heroku push day

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -90,10 +90,10 @@
 /*!*********************************!*\
   !*** ./frontend/entry_file.jsx ***!
   \*********************************/
-/*! no exports provided */
+/*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/prescottcarlson/Desktop/fullstack_project/prescloud/frontend/entry_file.jsx'");
+
 
 /***/ })
 
