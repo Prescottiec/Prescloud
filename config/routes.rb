93e7764098c5e5069bdc31fca77da3cd7f5d44a7
Prescott <carlsonprescott@gmail.com>
2020-01-16 19:02:20 -0800

    aws is set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:create]
+    resources :users, only: [:create]
+    resources :tracks, except: [:new, :edit]
     resource :session, only: [:create, :destroy, :show]
   end
   root to: "static_pages#root"
